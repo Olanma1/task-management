cd6ce26e6ee2dd2c1d035687dbc1c25e4fc6d42d
Olanma1 <olanmamary2@gmail.com>
2023-05-19 21:20:37 +0200

    Installed tailwind css for styling

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h1>Create a project</h1>
     <form method="POST" action="/">
         @csrf
@@ -29,5 +23,4 @@
             </div>
         </div>
     </form>
-</body>
-</html>
+    @endsection
